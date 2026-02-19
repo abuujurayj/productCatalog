b4f2665e548de7f4d9a58d7d1ee968e1bcff0816
sBuruhan <buruhanswabur@gmail.com>
2026-02-19 11:56:30 +0300

    tested successfully

--- setup-emulator.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export SPANNER_EMULATOR_HOST=localhost:9010
-
-# Create instance
-gcloud spanner instances create test-instance \
-    --config=emulator-config --description="Test Instance" --nodes=1
-
-# Create database
-gcloud spanner databases create catalog-db --instance=test-instance
